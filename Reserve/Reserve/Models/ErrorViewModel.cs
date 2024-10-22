5434302699a9c81002f21c03a6bb414bd97655e2
jafarmotlagh <jafarmotlagh820@gmail.com>
2024-10-22 04:49:52 -0700

    initial commit

---
+++ Reserve/Reserve/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Reserve.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
